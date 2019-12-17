f4c036827f2baf3027623f3a2c5eb0ee93edde5f
ankithakannad <akannad@amnh.org>
2019-12-17 13:20:20 -0500

    added in 12/17 files

---
+++ 191217_contourplotting/sample_script.py
@@ -0,0 +1,15 @@
+#Import packages
+
+#Create functions
+def hello(name):
+	print('Hello',name)
+
+#Main script
+def main():
+	
+	name = 'Beyonce'
+	hello(name)
+
+#Run script
+if __name__ == '__main__':
+	main()
