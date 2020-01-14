e5eede94d52c8c05247ff932c416b57b215de8af
ankithakannad <akannad@amnh.org>
2020-01-14 14:06:44 -0500

    edited script

--- scripts/plot_annual_sst_anomaly.py
+++ scripts/plot_annual_sst_anomaly.py
@@ -9,12 +9,12 @@ import sst_anomaly_functions
 #Main script
 def main():
 
-	#Step 1: Import data file
+	
 	dataPath = '/Users/hellenfellow/Dropbox (AMNH)/BridgeUP_ClimateCoders/Data'
 	os.chdir(dataPath)
 	data = Dataset('compressed_soda3.12.2_mn_ocean_reg_2015.nc')
 
-	#Step 2: Save the necessary variables
+	
 	lon = data.variables['xt_ocean'][:]
 	lat = data.variables['yt_ocean'][:]
 	time = data.variables['time'][:]
@@ -22,14 +22,14 @@ def main():
 	temp = data.variables['temp'][:]
 	salt = data.variables['salt'][:]
 
-	#Step 3: Slice data to dimensions we want
+	
 	condition_depth = depth <= 20
 	temp_sliced = temp[:,condition_depth,:,:]
 
-	#Step 4: Find mean SST
+	
 	temp_mean = temp_sliced.mean()
 	
-	#Step 5: Find SST anomaly
+	
 	temp_anom = temp[:,condition_depth,:,:] - temp_mean
 
 #Execute main script
