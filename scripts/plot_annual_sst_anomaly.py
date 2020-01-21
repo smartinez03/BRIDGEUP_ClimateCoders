dfcf5640d307d9c9de15fada434ab85de29bd75f
ankithakannad <akannad@amnh.org>
2020-01-21 12:26:38 -0500

    add files

--- scripts/plot_annual_sst_anomaly.py
+++ scripts/plot_annual_sst_anomaly.py
@@ -12,7 +12,7 @@ def main():
 	#Navigates to our data folder
 	dataPath = '/Users/hellenfellow/Dropbox (AMNH)/BridgeUP_ClimateCoders/Data'
 	os.chdir(dataPath)
-	data = Dataset('compressed_soda3.12.2_mn_ocean_reg_2015.nc')
+	data = Dataset('compressed_soda3.12.2_mn_ocean_reg_2017.nc')
 
 	# Setting variables
 	lon = data.variables['xt_ocean'][:]
@@ -31,6 +31,9 @@ def main():
 	
 	#Calculate anomalies
 	temp_anom = temp[:,condition_depth,:,:] - temp_mean
+	#temp_anom = (12,2,330,720)
+	temp_anom_yr = temp_anom.mean(axis = 0)
+	print(temp_anom_yr.shape)
 
 #Execute main script
 if __name__ == '__main__':
